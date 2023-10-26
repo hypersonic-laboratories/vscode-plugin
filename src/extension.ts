ee68f178deb603927bc41407134f8b53904528da
mai1015 <i@mai1015.com>
2023-10-26 17:00:37 -0400

    feat: update comment

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,3 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { CancellationToken, Hover, Position, ProviderResult, TextDocument } from 'vscode';
 
@@ -28,17 +26,7 @@ function provideHover(doc: TextDocument, pos: Position, tok: CancellationToken):
 	return null;
 }
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "helix-script" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('helix-script.addLuaLibrary', () => {
 		// Get the current workspace folder
         let workspaceFolder = vscode.workspace.workspaceFolders?.[0];
@@ -66,7 +54,6 @@ export function activate(context: vscode.ExtensionContext) {
         } else {
             vscode.window.showInformationMessage('Path is already added.');
         }
-		// vscode.window.showInformationMessage('Hello World from HELIX Script!');
 	});
 
 	context.subscriptions.push(disposable);
